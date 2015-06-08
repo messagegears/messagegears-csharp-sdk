99a6b5f44ac0701baf6313c41ca6b1d90cbf810f
Tim Davis <tim.davis@messagegears.com>
2015-06-08 15:21:53 +0000

    Project file changes

---
+++ MessageGearsSDK/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MessageGears")]
+[assembly: AssemblyDescription("The MessageGears SDK")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MessageGears")]
+[assembly: AssemblyProduct("MessageGearsSDK")]
+[assembly: AssemblyCopyright("MessageGears 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("94620ce0-5e47-462e-a2a8-891a3e26cce2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("4.5.2.0")]
+[assembly: AssemblyFileVersion("4.5.2.0")]
