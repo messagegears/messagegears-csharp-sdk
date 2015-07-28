28922bc6ac3216b8a58f31cdc9cd3090a4d7a612
Drew Stevens <drew.stevens@messagegears.com>
2015-07-28 16:28:02 -0400

    Fixing defects

--- MessageGearsSDK/MessageGears/MessageGearsClient.cs
+++ MessageGearsSDK/MessageGears/MessageGearsClient.cs
@@ -533,7 +533,7 @@ namespace MessageGears
             // deserialize response into TransactionalContent
             XmlSerializer serializer = new XmlSerializer (typeof(TransactionalContent));
             using (XmlTextReader sr = new XmlTextReader (new StringReader (response))) {
-                BulkJobSummaryResponse objectResponse = (BulkJobSummaryResponse)serializer.Deserialize (sr);
+                TransactionalContent objectResponse = (BulkJobSummaryResponse)serializer.Deserialize (sr);
                 if(objectResponse.Result.Equals(Result.REQUEST_SUCCESSFUL))
                 {
                     log.Info("Transactional Content Retrieval successfully processed: " + objectResponse.RequestId);
@@ -737,14 +737,14 @@ namespace MessageGears
             PrintResponse(response.Result, response.RequestErrors);
             if (response.Result == Result.REQUEST_SUCCESSFUL) {                
                 if (response.RenderErrors == null) {
-                    Console.WriteLine("FromName: " + response.PreviewContent.FromName +
-                                      " <" + response.PreviewContent.FromAddress + ">");
-                    Console.WriteLine("Subject: " + response.PreviewContent.SubjectLine);
-                    Console.WriteLine("HtmlContent: " + response.PreviewContent.HtmlContent);
-                    Console.WriteLine("TextContent: " + response.PreviewContent.TextContent);
+                    Console.WriteLine("FromName: " + response.FromName +
+                                      " <" + response.FromAddress + ">");
+                    Console.WriteLine("Subject: " + response.SubjectLine);
+                    Console.WriteLine("HtmlContent: " + response.HtmlContent);
+                    Console.WriteLine("TextContent: " + response.TextContent);
                     Console.WriteLine("Attachments:");
                     foreach(Attachment attachment in response.Attachment) {
-                        Console.WriteLine("            Name: " attachment.NAme);
+                        Console.WriteLine("            Name: " attachment.Name);
                         Console.WriteLine("    Content-Type: " attachment.ContentType);
                     }
                 } else {
