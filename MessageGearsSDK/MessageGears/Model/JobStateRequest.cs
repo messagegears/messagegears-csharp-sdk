1df05be5539aecc609e6565f84f6731cf3eea293
Drew Stevens <drew.stevens@messagegears.com>
2015-09-15 11:18:30 -0400

    Introducing needed compoents for JobStatus endpoint

---
+++ MessageGearsSDK/MessageGears/Model/JobStateRequest.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace MessageGears.Model
+{
+    /// <summary>
+    /// Used to submit the request to set job status in the MessageGears service.
+    /// </summary>
+    public class JobStateRequest
+    {
+
+        /// <summary>
+        /// The Action Code that indicates the API being called to the MessageGears system.
+        /// </summary>
+        public const string Action = "JobStatus";
+        
+        /// <summary>
+        /// The request id of the job whose status is to be set.
+        /// </summary>
+        public string JobRequestId { get; set; }
+
+        /// <summary>
+        /// The numeric representation of the state the job is to be set to.
+        /// </summary>
+        public string JobStatus { get; set; }
+
+
+    }
+}
+
---
+++ MessageGearsSDK/MessageGears/Model/JobStateRetrievalRequest.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace MessageGears.Model
+{
+    /// <summary>
+    /// Used to submit the request to retrieve the job status in the MessageGears service.
+    /// </summary>
+    public class JobStateRetrievalRequest
+    {
+
+        /// <summary>
+        /// The Action Code that indicates the API being called to the MessageGears system.
+        /// </summary>
+        public const string Action = "JobStatusRetrieval";
+        
+        /// <summary>
+        /// The request id of the job whose status is to be set.
+        /// </summary>
+        public string JobRequestId { get; set; }
+
+    }
+}
+
